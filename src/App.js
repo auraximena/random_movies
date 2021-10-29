2607e2fbce5a898954f28bf2bb2a0c60dbfad746
Aura <auraximenagomez6@gmail.com>
2021-10-29 14:24:29 -0500

    se agrega título y pestañas

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-import {Button} from 'reactstrap'
+import { Button, Nav, NavItem, NavLink, TabContent, TabPane, Row, Col, CardTitle, CardText, Card } from 'reactstrap'
 import { useState } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.css'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import ObtenerPelicula from './ObtenerPelicula';
 
 
 function App() {
-  const [nombre, setNombre] = useState("");
-  const onCLickBoton = function (valor){
-    setNombre({nombre: valor})
-    alert("Eres el amor de mi vida "+ nombre)
-  }
+  // const [nombre, cambiarNombre] = useState("");
 
-  const obtenerNombre = function(valor){
-    setNombre(valor.target.value)
-  }
-  
+  // const onCLickBoton = function (valor){
+  //   debugger;
+  //   cambiarNombre({nombre: valor})
+  //   alert("Eres el amor de mi vida "+ nombre)
+  // }
+
+  // const obtenerNombre = function(valor){
+  //   cambiarNombre(valor.target.value)
+
+
   return (
-    <div className="App">
-      <h1>MI PRIMERA APLICACIÓN</h1>
-      <label>Ingresa tu nombre:</label><br/>
-      <input type="text" value={nombre} onChange={obtenerNombre}></input><br/>
-      <Button onClick={onCLickBoton}>Click!</Button>
-      {/* <div>
-        Nombre: {nombre}
-      </div> */}
+    // éste es el título de la página
+    <div className="body">
+      <Router>
+        <Route exact path="/" component={ObtenerPeliculareact }></Route>
+      </Router>
+      <div className="title">
+        <h1>RANDOM MOVIES</h1>
+        <div>
+          <p>¡Deja de discutir con tu pareja por cuál película escoger!</p>
+        </div>
+      </div>
+      <div>
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Obtener Película</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Lista de Películas</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/ObtenerPelicula.js
@@ -0,0 +1,36 @@
+const ObtenerPelicula = () => (
+
+  <section>
+    <h3>Holi</h3>
+  </section>
+
+
+
+
+
+
+
+    // const [nombre, setNombre] = useState("");
+    // const onCLickBoton = function (valor){
+    //   setNombre({nombre: valor})
+    //   alert("Eres el amor de mi vida "+ nombre)
+    // }
+  
+    // const obtenerNombre = function(valor){
+    //   setNombre(valor.target.value)
+    // }
+    
+    // return (
+    //   <div className="App">
+    //     <h1>MI PRIMERA APLICACIÓN</h1>
+    //     <label>Ingresa tu nombre:</label><br/>
+    //     <input type="text" value={nombre} onChange={obtenerNombre}></input><br/>
+    //     <Button onClick={onCLickBoton}>Click!</Button>
+    //     {/* <div>
+    //       Nombre: {nombre}
+    //     </div> */}
+    //   </div>
+    // );
+)
+
+export default ObtenerPelicula;
